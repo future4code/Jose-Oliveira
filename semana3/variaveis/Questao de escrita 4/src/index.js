94620cb6fb7782da7cca553b7e526b916a4a0418
Rafael-Oliveira <rafaelllmoliveira@gmail.com>
2020-09-28 13:17:28 -0300

    resposta da atividade de variaveis

---
+++ semana3/variaveis/Questao de escrita 4/src/index.js
@@ -0,0 +1,5 @@
+let perguntas = ["você está com fome?","você esta cansado?","você já terminou a atividade de hoje?"]
+let respostas = ["false","true","true"]
+console.log(perguntas[0], respostas[0])
+console.log(perguntas[1], respostas[1])
+console.log(perguntas[2], respostas[2])
