d2d81f8c7008e2338b9ff9cafd830fc25dde2937
Rafael-Oliveira <rafaelllmoliveira@gmail.com>
2020-10-01 14:00:50 -0300

    exercicios de leitura e escrita de codigo 01/10

---
+++ semana3/loops/js-vanilla-template/src/index.js
@@ -0,0 +1,61 @@
+
+/*Questão 1 interpretação
+o codigo esta verificando o valor de i e se for menor que 5 ele soma 1 e adiciona a variavel valor
+a soma" a ser impressa sera "10"
+*/
+
+/*Questão 2 interpretação
+a.19
+21
+23
+25
+27
+30
+
+b.Sim, como no seguinte codigo:
+let i = 0
+const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (let numero of lista) {
+  if (numero > 18) {
+        console.log("Índice: ", i,"numero:",numero)
+    }
+    i++
+}
+
+*/
+
+/*questão 3 escrita
+const lista = [1,2,3,4,5,6,7,8,9,10]
+
+a.for (let numero of lista) {
+    console.log(numero)
+}
+
+b.for (let numero of lista) {
+    console.log(numero/10)
+}
+
+c.for (let numero of lista) {
+    if(numero%2 === 0){
+console.log(numero)
+    }
+}
+
+d.let valor = 0
+for(let i=0; i <lista.length; i++){
+    valor = i
+    console.log("O elemento do index",i,"é",lista[i])
+}
+
+e.let valorMaximo = 0
+let valorMinimo = 0
+for(numero of lista){if(numero < valorMinimo){
+    valorMinimo = numero
+}
+    if(numero > valorMaximo){
+        valorMaximo = numero
+    }
+}
+console.log(valorMinimo)
+console.log(valorMaximo)
+*/
