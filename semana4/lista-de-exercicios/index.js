f615db0225cc77db15956b49c15a3c07ca205be2
José Rafael <rafaelllmoliveira@gmail.com>
2020-10-09 11:24:23 -0300

    Questão 1 - interpretação

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,5 @@
+/*Questão 1 - interpretação
+a constante meu dinheiro ira chamar a função conversor de moeda que ira receber 100 dolares como valor em
+seguida a função ira pedir por prompt o valor da cotação atual do dolar e em seguida fara a conversão e ira
+imprimir no console
+*/
