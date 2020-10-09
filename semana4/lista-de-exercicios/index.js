43f91dcddb3910808939ed45dc2b4a31af29e804
José Rafael <rafaelllmoliveira@gmail.com>
2020-10-09 11:30:27 -0300

    Questão 2 - interpretação

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -3,3 +3,9 @@ a constante meu dinheiro ira chamar a função conversor de moeda que ira recebe
 seguida a função ira pedir por prompt o valor da cotação atual do dolar e em seguida fara a conversão e ira
 imprimir no console
 */
+
+/*Questão 2 - interpretação
+as constantes novoMontante e segundoMontante irão chamar a função investe Dinheiro inserindo as informações
+ações, 150 e tesouro Direto, 200 respectivamente as funções em seguida irão verificar o tipo do investimento
+e multiplicar o valor pela correspondente constante e em seguida ira imprimir os resultados
+*/
