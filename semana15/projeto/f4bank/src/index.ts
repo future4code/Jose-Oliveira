e5fd2e9f2a10951daaf6d55e3160a8727f25ed5a
RafaelOliveira215 <rafaelllmoliveira@gmail.com>
2021-01-08 21:54:51 -0300

    adicionei as funcionalidades: adicionar saldo, pagar conta e transferencia interna

--- semana15/projeto/f4bank/src/index.ts
+++ semana15/projeto/f4bank/src/index.ts
@@ -26,8 +26,15 @@ const users:user[]=[
     name:"teste",
     CPF:6121736585,
     birthDate:"09/06/1993",
-    balance:0,
+    balance:500,
     transactions:[]
+},
+{
+    name:"teste2",
+    CPF:6121736478,
+    birthDate:"09/06/1985",
+    balance:0,
+    transactions:[]  
 }
 ]
 
@@ -68,6 +75,77 @@ app.get("/user/balance",(req:Request,res:Response)=>{
             user=> user.CPF === req.body.CPF
         )
         res.status(200).send("Seu saldo atual é: "+result.balance)
+    }
+})
+
+app.put("/user/addbalance",(req:Request,res:Response)=>{
+    const data ={
+        name:req.body.name,
+        CPF:req.body.CPF,
+        value:req.body.value
+    }
+    if(data.name && data.CPF && data.value){
+        const index = users.findIndex(
+            user => user.CPF === Number(req.body.CPF)
+        )
+        users[index].balance = Number(users[index].balance) + Number(req.body.value)
+      res.status(200).send("Saldo adicionado com sucesso")
+    }
+
+})
+
+app.put("/user/payment",(req:Request,res:Response)=>{
+    const data = {
+        CPF:req.body.CPF,
+        name:req.body.name,
+        value:req.body.value,
+        description:req.body.description,
+        date:req.body.date
+    }
+
+
+    let date = data.date
+    if(!data.date){
+        date= moment(new Date()).add(1,'days').format('DD/MM/YYYY')
+    }
+    const daysToPayment = moment(`${date}`,"DD/MM/YYYY").fromNow()
+    const index = users.findIndex(
+        user => user.CPF === Number(req.body.CPF)
+     )
+
+    if((Number(daysToPayment.substring(3,4))>=1 || date === moment(new Date()).format('DD/MM/YYYY')) && users[index].balance >= req.body.value ){
+    users[index].balance = Number(users[index].balance) - Number(req.body.value)
+
+        res.status(200).send("Pagamento efetuado com sucesso")
+    }else{
+        res.status(406).send("Dados preenchidos incorretamente")
+    }
+})
+
+app.put("/user/transfer",(req:Request,res:Response)=>{
+
+    const originindex:number = users.findIndex(
+        user => user.CPF === Number(req.body.originCPF)
+    )
+    const targetindex:number = users.findIndex(
+        user => user.CPF === Number(req.body.targetCPF)
+    )
+
+    const originUser ={
+        name:req.body.originName,
+        CPF:req.body.originCPF,
+        value:req.body.value,
+        balance:users[originindex].balance
+    }
+    const targetUser ={
+      name:req.body.targetName,
+      CPF: req.body.targetCPF
+    }
+
+    if(originUser.balance >= originUser.value && originUser.name && originUser.CPF && originUser.value && originUser.balance && targetUser.name && targetUser.CPF){
+        users[originindex].balance = Number(users[originindex].balance) - Number(originUser.value)
+        users[targetindex].balance = Number(users[targetindex].balance) + Number(originUser.value)
+        res.status(200).send("transferencia concluida")
     }
 })
 
