3f6cb0e67cb15afe343bc772583a0f85574057e9
RafaelOliveira215 <rafaelllmoliveira@gmail.com>
2021-03-15 16:10:28 -0300

    exercicios de logica

---
+++ semana24/aula-hash/compression.ts
@@ -0,0 +1,25 @@
+const stringCompression = (input) => {
+    const substrings = []
+    let lastChar = input[0]
+    let charCount = 0
+  
+    for (const char of input) {
+      if (char !== lastChar) {
+        substrings.push(lastChar + charCount)
+        lastChar = char
+        charCount = 0
+      }else{
+      charCount++
+    }
+    }
+  
+    substrings.push(lastChar + charCount)
+    let result = ""
+    for (const key of substrings) {
+      result += key
+    }
+  
+    return result.length > input.length ? input : result
+  }
+
+  console.log(stringCompression("aaaaaabcd"))
